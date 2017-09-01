1f108535ceace7091b6f6a0e7009e3295930f94c
Ben Slabbert <ben@hex.co.za>
2017-09-01 17:46:20 +0200

    initial commit

---
+++ server/main.js
@@ -0,0 +1,6 @@
+import {greetUser} from './../imports/utils';
+import {add} from './../imports/math';
+
+console.log('Log from client/main/js');
+console.log(greetUser());
+console.log('adding: ', add(1, 2));
