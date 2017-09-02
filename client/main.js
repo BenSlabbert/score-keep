4651fe576a722b7fd3aa7274449c014fac4f79cd
Ben Slabbert <ben@hex.co.za>
2017-09-02 09:39:20 +0200

    adding a cont list to iterate over to produce a list of jsx to be rendered.

--- client/main.js
+++ client/main.js
@@ -2,6 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Meteor } from 'meteor/meteor';
 
+const players = [{
+  _id: '1',
+  name: 'Ben',
+  score: 99
+}, {
+  _id: '2',
+  name: 'James',
+  score: 2
+}, {
+  _id: '3',
+  name: 'Matt',
+  score: 104
+}];
+
+const renderPlayers = function (playersList) {
+
+  return newNumbers = playersList.map(function (player) {
+    return <p key={player._id}>{player.name + ':'} {player.score} point(s)</p>;
+  });
+};
+
 Meteor.startup(function () {
   let title = 'Score Keep';
   let name = 'Ben';
@@ -11,6 +32,9 @@ Meteor.startup(function () {
       <h1>{title}</h1>
       <p>Hello {name}!</p>
       <p>another</p>
+
+      {/* display jsx list */}
+      {renderPlayers(players)}
     </div>
   );
 
