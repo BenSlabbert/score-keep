4c77fe73077ca7c1a774361881c7057a9da6ea96
Ben Slabbert <ben@hex.co.za>
2017-09-03 01:10:36 +0200

    adding flip move prop

--- imports/ui/PlayerList.js
+++ imports/ui/PlayerList.js
@@ -24,7 +24,7 @@ export default class PlayerList extends React.Component {
     render() {
         return (
             <div>
-                <FlipMove>
+                <FlipMove maintainContainerHeight={true}>
                     {this.renderPlayers()}
                 </FlipMove>
             </div>
