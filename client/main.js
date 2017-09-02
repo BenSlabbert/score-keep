ecee554477baf134452d55bdc2df9becd3232bd3
Ben Slabbert <ben@hex.co.za>
2017-09-02 08:55:57 +0200

    using jsx to render template to the screen

--- client/main.js
+++ client/main.js
@@ -1,4 +1,10 @@
-import someDef from './../imports/utils';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Meteor} from 'meteor/meteor';
 
-console.log('Log from client/main/js');
-console.log(someDef);
+Meteor.startup(function () {
+  let name = 'Ben';
+  let jsx = <p>Hello {name}!</p>;
+
+ReactDOM.render(jsx, document.getElementById('app'));
+});
