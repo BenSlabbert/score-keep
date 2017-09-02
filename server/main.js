247ba66824968c00570cc1b0bfacccaaa094acff
Ben Slabbert <ben@hex.co.za>
2017-09-02 13:53:12 +0200

    adding programmer extension class

--- server/main.js
+++ server/main.js
@@ -3,27 +3,58 @@ import { Players } from './../imports/api/players';
 
 Meteor.startup(() => {
     class Person {
-        constructor(name = 'Anon', age = 0){
+        constructor(name = 'Anon', age = 0) {
             this.name = name;
             this.age = age;
         }
 
         getGreeting() {
-            return `Hi! I am ${this.name}.`; 
+            return `Hi! I am ${this.name}.`;
         }
 
-        getPersonDescription(){
+        getPersonDescription() {
             return `${this.name} is ${this.age} years old.`;
         }
     }
 
-    let me = new Person('Ben', 24);
-    console.log(me);
+    class Employee extends Person {
+
+        constructor(name, age, title) {
+            super(name, age)
+            this.title = title;
+        }
+
+        getGreeting() {
+            if (this.title) {
+                return `Hi, I am ${this.name} I work as a ${this.title}`;
+            } else {
+                return super.getGreeting();
+            }
+        }
+
+        hasJob() {
+            return !!this.title;
+        }
+    }
+
+    // programmer class
+    // extend from person
+    // name, age, preferredLanguage has default 'aasembly'
+    // override getGreeting 
+        // hi i am {name} I am a {language} developer
+
+    class Programmer extends Person {
+
+        constructor (name, age, preferredLanguage = 'Assembly'){
+            super(name, age);
+            this.preferredLanguage = preferredLanguage;
+        }
+
+        getGreeting(){
+            return `Hi! I am ${this.name}, I am a ${this.preferredLanguage} developer.`;
+        }
+    }
+
+    let me = new Programmer('Ben', 25, 'Java');
     console.log(me.getGreeting());
-    console.log(me.getPersonDescription());
-    
-    me = new Person('tim');
-    console.log(me);
-    console.log(me.getGreeting());
-    console.log(me.getPersonDescription());
 });
